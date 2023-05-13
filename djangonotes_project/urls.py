7551fdf71db304e9cc24ddf9f340af4f878114c0
mejkej <krus3@icloud.com>
2023-05-13 16:48:44 +0000

    added ur;s.py for the entry app started on the forms and views

--- djangonotes_project/urls.py
+++ djangonotes_project/urls.py
@@ -12,10 +12,14 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    path('signup/', signup_view, name="signup")
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('entry_app/', include('entry_app.urls')),
+    
 ]
--- entry_app/views.py
+++ entry_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from .forms import UserCreationForm
 
 # Create your views here.
 def signin_view(request):
-    return render (request, entry_app/signin.html)
+    return render(request, 'signin')
+
+def signup_view(request):
+    return render(request, 'signup')
