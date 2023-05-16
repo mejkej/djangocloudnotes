6f25d0b7d5eff2c8d3d2250b5a0d86f3ec2c7884
mejkej <krus3@icloud.com>
2023-05-16 21:11:21 +0000

    forms and views.py files contains the first rough sketch but will need some polishing

--- entry_app/forms.py
+++ entry_app/forms.py
@@ -3,5 +3,57 @@ from django.core.exceptions import ValidationError
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
 
-class CustomSignUpForm(forms.ModelForm):
-    username = forms.CharField(widget=forms.PasswordInput, max_length=20, min_length=5)
+class SignUpForm(forms.ModelForm):
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), 
+        max_length=20, 
+        min_length=5
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}), 
+        max_length=20, 
+        min_length=5
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ['username']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username', 'min_length': 3, 'max_length': 20})
+        }
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if CustomUser.objects.filter(username=username).exists():
+            raise forms.ValidationError('Username not available.')
+        return username
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Passwords do not match')
+
+        validate_password(password2)
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+class SignInForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}), 
+        max_length=20, 
+        min_length=3
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}), 
+        max_length=20, 
+        min_length=5
+    )
--- entry_app/views.py
+++ entry_app/views.py
@@ -1,1 +1,34 @@
-from .forms import 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import SignUpForm, SignInForm
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'entry_app/signup.html', {'form': form})
+
+
+def signin_view(request):
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('main')  # Redirect to main_app or another page after successful login
+            else:
+                form.add_error(None, 'Invalid username or password')
+    else:
+        form = SignInForm()
+
+    return render(request, 'entry_app/signin.html', {'form': form})
+
